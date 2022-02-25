075490d6a6577f688e7d5e1200230de107cdfa25
SuchithraGowdaR <suchithragowda253@gmail.com>
2022-02-25 15:26:29 +0530

    example programs of datatypes

---
+++ datatype/datatypepracticed.java
@@ -0,0 +1,24 @@
+class flash{
+
+	public static void main(String[]ar){
+	 
+	
+	byte note=100;
+	 System.out.println(note);
+	 
+	short swich=10000;
+	 System.out.println(swich);
+
+	long light=1234567890987l;
+	 System.out.println(light);
+
+	double time=8.4555555;
+	 System.out.println(time);
+
+	char suchi='g';
+	 System.out.println(suchi);
+
+	boolean coffee=true;
+	 System.out.println(coffee);
+	}
+}
---
+++ datatype/lap.java
@@ -0,0 +1,21 @@
+class gowda{
+	public static void main(String []ar){
+	 byte bag=120;
+		System.out.println(bag);
+	 short bike=-4218;
+                System.out.println(bike);
+	 int piNCod=33560;
+		System.out.println(piNCod);
+	 long mobLEno=9742888513l;
+		System.out.println(mobLEno);
+	 float laPLnth=5.255555f;
+		System.out.println(laPLnth); 
+	 double key=9.9999999999;
+		System.out.println(key);
+	 char fOOd='p';
+		System.out.println(fOOd);
+	 boolean graDCArd=true;
+		System.out.println(graDCArd);
+	 System.out.print("pulav");
+	}
+}
